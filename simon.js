425ca01f827f8de361ada8e76f2fd7d237de9ebc
Ick Hyun Kwon <kwonih1020@gmail.com>
2017-04-06 16:39:07 -0400

    done

--- simon.js
+++ simon.js
@@ -1,17 +1,16 @@
-
 // Basic steps to makes game run
 function newGame() {
-      resetGame();
-      randomPattern();
-      playPattern();
-    }
+    resetGame();
+    randomPattern();
+    playPattern();
+}
 
 let check = false;
 
 // create click function to start new game.
 $("#innerCircle").click(function() {
     if (check === false) {
-    return newGame();
+        return newGame();
     }
 });
 
@@ -19,91 +18,91 @@ let stage = 1;
 
 // Created function for resetting game,
 function resetGame() {
-  $(".button").unbind(); //makes all events click off
-  // after all events off, display shows h1, p, #stage.
-  $("h1").html("Simon Game");
-  $("p").html("Click here to Start");
-  $("#stage").html("Stages: " + stage);
+    $(".button").unbind(); //makes all events click off
+    // after all events off, display shows h1, p, #stage.
+    $("h1").html("Simon Game");
+    $("p").html("Click here to Start");
+    $("#stage").html("Stages: " + stage);
 }
 
 let pattern = [];
 // Generating random pattern
 function randomPattern() {
-  let value = generateRandomNum(1, 4);
-  pattern.push(value);
-      }
+    let value = generateRandomNum(1, 4);
+    pattern.push(value);
+}
 
 function generateRandomNum(x, y) {
-  //using Math.random method to make each stage random pattern
-  return Math.floor(Math.random() * (y - x + 1)) + x;
-      }
+    //using Math.random method to make each stage random pattern
+    return Math.floor(Math.random() * (y - x + 1)) + x;
+}
 
 function playPattern() {
-  for (let i = 0; i < pattern.length; i++) {
-    setTimeout(flashSquare, i * 700);
-        }
-      }
+    for (let i = 0; i < pattern.length; i++) {
+        setTimeout(flashSquare, i * 700);
+    }
+}
 
 let usedPattern = [];
 
 function flashSquare() {
-  let item = pattern.pop();
+    let item = pattern.pop();
     check = true;
     document.getElementById("firstSound").play();
     //play sound when the square flashes
     //pops and remove (last_item) from array
     $("#" + item).animate({
-      opacity: ".5"
-        }, 300).animate({
-      opacity: 1.5
-        }, 400);
+        opacity: ".5"
+    }, 300).animate({
+        opacity: 1.5
+    }, 400);
     usedPattern.push(item);
     //add removed item to  end of usedPattern
     if (0 >= pattern.length) {
-      // click event
-    return buttonClicks();
+        // click event
+        return buttonClicks();
     }
 }
 
 function buttonClicks() {
-  $(".button").click(function() {
-    // check if clicked square is right one
-    let item = usedPattern.shift(); //remove first step from usedPattern array
-    let buttonId = $(this).attr("id"); //return the value of an attribute.
-  $(this).animate({
-    opacity: ".5"
-  }, 300).animate({
-    opacity: 1.5
-  }, 400)
-  if (item == buttonId) { //when colors matched
-    pattern.push(item);
-    //back to pattern array
-    if (0 >= usedPattern.length) {
-      stage++; //stages are increasing
-      $("#stage").html("Stages: " + stage);
-      $(".button").unbind();
-      randomPattern();
-      setTimeout(playPattern, 800);
-    }
-      document.getElementById("secondSound").play();
-      //play sound when user clicked right square
-  } else {
-    //when game over, restart the game.
-    check = false;
-    document.getElementById("wrongSound").play();
-    //play sound when user clicked wrong square.
-    $("h1").html("Game Over").css({
-      fontSize: 20,
-      marginBottom: 15,
-      paddingTop: 40
+    $(".button").click(function() {
+        // check if clicked square is right one
+        let item = usedPattern.shift(); //remove first step from usedPattern array
+        let buttonId = $(this).attr("id"); //return the value of an attribute.
+        $(this).animate({
+            opacity: ".5"
+        }, 300).animate({
+            opacity: 1.5
+        }, 400)
+        if (item == buttonId) { //when colors matched
+            pattern.push(item);
+            //back to pattern array
+            if (0 >= usedPattern.length) {
+                stage++; //stages are increasing
+                $("#stage").html("Stages: " + stage);
+                $(".button").unbind();
+                randomPattern();
+                setTimeout(playPattern, 800);
+            }
+            document.getElementById("secondSound").play();
+            //play sound when user clicked right square
+        } else {
+            //when game over, restart the game.
+            check = false;
+            document.getElementById("wrongSound").play();
+            //play sound when user clicked wrong square.
+            $("h1").html("Game Over").css({
+                fontSize: 20,
+                marginBottom: 15,
+                paddingTop: 40
+            });
+            $("p").html("Click here to Restart").css({
+                fontSize: 10,
+                paddingTop: 35
+            });
+            // clear pattern arrays
+            pattern = [];
+            usedPattern = [];
+        }
     });
-    $("p").html("Click here to Restart").css({
-      fontSize: 10,
-      paddingTop: 35
-    });
-    // clear pattern arrays
-    pattern = [];
-    usedPattern = [];
-  }
-  });
 }
